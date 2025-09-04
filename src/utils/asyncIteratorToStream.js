d3f8e20024a6fbc089583869197b71dffa6e4424
James Prevett <jp@jamespre.dev>
2025-09-04 10:45:55 -0500

    Remove redundant TS lib Remove unneeded polyfill Update engine and @types/node

--- src/utils/asyncIteratorToStream.js
+++ src/utils/asyncIteratorToStream.js
@@ -1,11 +1,12 @@
-import { forAwait } from './forAwait.js'
+import { PassThrough } from 'readable-stream'
 
 export function asyncIteratorToStream(iter) {
-  const { PassThrough } = require('readable-stream')
   const stream = new PassThrough()
-  setTimeout(async () => {
-    await forAwait(iter, chunk => stream.write(chunk))
+  queueMicrotask(async () => {
+    for await (const chunk of iter) {
+      stream.write(chunk)
+    }
     stream.end()
-  }, 1)
+  })
   return stream
 }
